fd7015cf673355b3965fcc75acdfc6257266d12c
SaiChintada <chintadasai51758@gmail.com>
2026-05-03 22:56:00 +0530

    Added hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Hi, I'm Sai Kiran</h1>
+      <h2>Full Stack Developer</h2>
+      <p>I build modern web applications using React & FastAPI</p>
+    </section>
+  );
+}
+
+export default Hero;
