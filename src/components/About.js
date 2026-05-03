ef3d59ad64f9edde7f2e105ca7525909df60e999
SaiChintada <chintadasai51758@gmail.com>
2026-05-03 22:57:29 +0530

    Added about section

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        I am a Computer Science student passionate about building full-stack
+        applications using modern technologies.
+      </p>
+    </section>
+  );
+}
+
+export default About;
