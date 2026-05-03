ceb7896eda2687a20b2437c4a5c64f273778d724
SaiChintada <chintadasai51758@gmail.com>
2026-05-03 22:58:27 +0530

    Added projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,24 @@
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+
+      <div>
+        <h3>Expense Tracker</h3>
+        <p>React + FastAPI + PostgreSQL</p>
+      </div>
+
+      <div>
+        <h3>Bus Tracking System</h3>
+        <p>React + Maps API</p>
+      </div>
+
+      <div>
+        <h3>Banking API System</h3>
+        <p>FastAPI + SQL</p>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
